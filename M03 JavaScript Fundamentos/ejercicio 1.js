7d60eeb5bff83528422d47daadea5b5f5c4cc493
rominabrutus <141417715+rominabrutus@users.noreply.github.com>
2023-08-15 08:06:54 -0300

    tarea 01 modulo 03

---
+++ M03 JavaScript Fundamentos/ejercicio 1.js
@@ -0,0 +1,23 @@
+const { resta } = require("./Ejercicio 02")
+
+crea una variable tipo String
+
+const nuevoString = ";
+
+crea una variable de tipo Boolean
+
+const nuevoBoolean = false;
+
+crea una variable de tipo Number
+
+const nuevoNumero = 0;
+
+crea una variable de tipo resta
+
+const nuevaResta = 10 - 7 === 3;
+
+crea una variable de tipo multiplicacion 
+
+const nuevaMultiplicacion = 10 * 4 === 40;
+
+const nuevoModulo 21 % 5 === 1;
